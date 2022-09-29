f641f27cdac5fd26da0b35d1ba893bf1cbab9bf7
lor1cd <laurrains@gmail.com>
2022-09-30 00:02:11 +0200

    par classe + globalisation passage tab

--- utils/urls.py
+++ utils/urls.py
@@ -1,4 +1,8 @@
 
-
-def get_url(urls, field_value):                  
-        return list(filter(lambda p : p['name'] == field_value, urls))[0]['url']
+class Urls:
+      
+        def __init__(self, urls):            
+                self.urls = urls
+                
+        def get_url(self, field_value):                  
+                return list(filter(lambda p : p['name'] == field_value, self.urls))[0]['url']
