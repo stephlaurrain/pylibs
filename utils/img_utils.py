e0fa3e2e33f5cd922686e235d0c10b91fa5b9423
lor1cd <laurrains@gmail.com>
2022-10-26 01:30:45 +0200

    lib traitement images conversion png to webp

---
+++ utils/img_utils.py
@@ -0,0 +1,21 @@
+import os
+from PIL import Image
+from pathlib import Path
+
+def convert_to_webp(source, ext, rm_source=False):                               
+                destination = os.path.splitext(source)[0]+ext
+                image = Image.open(source)  
+                # print(destination)
+                image.save(destination, format="webp")  
+                if rm_source:
+                    os.remove(source)
+                return destination           
+        
+def convert_png_to_jpg():
+        paths = Path(f"{self.root_app}{os.path.sep}data{os.path.sep}results").glob("**/*.png")
+        today = datetime.now()
+        for path in paths:
+                destination = os.path.splitext(path)[0]+'.jpg'
+                image = Image.open(path)  
+                print(destination)
+                image.save(destination, format="webp")  
