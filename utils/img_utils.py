b41322e1828fa8399308ab02274d7a3803d08a5b
lor1cd <laurrains@gmail.com>
2022-10-26 01:44:17 +0200

    qualité

--- utils/img_utils.py
+++ utils/img_utils.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 from pathlib import Path
 
-def convert_to_webp(source, ext, rm_source=False, quality=90):                               
+def convert_to_webp(source, ext, rm_source=False, quality=100):                               
                 destination = f"{os.path.splitext(source)[0]}.{ext}"
                 image = Image.open(source)  
                 # print(destination)
@@ -16,12 +16,12 @@ if (command == "conv"):
     img_utils.convert_dir_to_webp(f"{self.root_app}{os.path.sep}data{os.path.sep}results", rm_source=True)
     exit()
 """
-def convert_dir_to_webp(source_path, rm_source=False):
+def convert_dir_to_webp(source_path, rm_source=False, quality=100):
         paths = Path(f"{source_path}").glob("**/*.png")
         for path in paths:
                 destination = os.path.splitext(path)[0]+'.jpg'
                 image = Image.open(path)  
                 print(destination)
-                image.save(destination, format="webp")  
+                image.save(destination, format="webp", quality=quality)    
                 if rm_source:
                     os.remove(path)
