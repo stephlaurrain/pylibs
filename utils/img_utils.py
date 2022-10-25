4654545c66ac63388845f7a43d4e4c58ba6267f5
lor1cd <laurrains@gmail.com>
2022-10-26 01:34:58 +0200

    qualité webp

--- utils/img_utils.py
+++ utils/img_utils.py
@@ -2,11 +2,11 @@ import os
 from PIL import Image
 from pathlib import Path
 
-def convert_to_webp(source, ext, rm_source=False):                               
-                destination = os.path.splitext(source)[0]+ext
+def convert_to_webp(source, ext, rm_source=False, quality=90):                               
+                destination = f"{os.path.splitext(source)[0]}.{ext}"
                 image = Image.open(source)  
                 # print(destination)
-                image.save(destination, format="webp")  
+                image.save(destination, format="webp", quality=quality)  
                 if rm_source:
                     os.remove(source)
                 return destination           
