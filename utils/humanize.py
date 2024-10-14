2867a96aa0f1d8c2eb68954c1e488aded583befd
stephlaurrain <stephjlorr1@gmail.com>
2024-10-14 23:45:13 +0200

    humanize with stop

--- utils/humanize.py
+++ utils/humanize.py
@@ -1,4 +1,5 @@
 import inspect
+import os 
 from time import sleep
 import random
 from utils.mydecorators import _error_decorator
@@ -12,7 +13,16 @@ class Humanize:
             self.wait = wait
             self.default_wait = default_wait
             self.trace = trace
+            self.root_app = os.getcwd()
       
+
+    def stop(self):
+        stopfile = f"{self.root_app}{os.path.sep}stop"
+        res = os.path.exists(stopfile)
+        if (res):
+                self.log.lg("=STOP THE BOT=")
+        return res
+
     @_error_decorator()
     def wait_human(self, offset=-1, tmp=-1):
         self.trace(inspect.stack())
@@ -27,4 +37,8 @@ class Humanize:
         random.random()
         res = random.randint(loffset, loffset+loffwait)
         self.log.lg(f"wait for {res} seconds")
-        sleep(res)
+        for i in range(res):
+              if self.stop():
+                    break
+              sleep(1)        
+        # sleep(res)
